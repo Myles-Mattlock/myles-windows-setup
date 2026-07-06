7f23ed3d2c774cf42bbe1e38cd33893ccc96a80d
Myles Mattlock <myles.mattlock@outlook.com>
2026-07-07 00:06:13 +0100

    fixes

--- Installer.ps1
+++ Installer.ps1
@@ -183,6 +183,17 @@ foreach ($Reg in $RegistrySettings) {
     Set-ItemProperty -Path $Reg.Path -Name $Reg.Name -Value $Reg.Value -Type $Reg.Type
 }
 
+# disable powershell7 telemetry
+Write-Host "Disabling PowerShell 7 Telemetry..." -ForegroundColor Yellow
+
+# Sets the environment variable globally at the Machine level
+[Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
+
+# Updates the current running script session immediately so you don't have to restart the terminal
+$env:POWERSHELL_TELEMETRY_OPTOUT = '1'
+
+Write-Host "PowerShell 7 Telemetry disabled successfully." -ForegroundColor Green
+
 # Remove specific PeriodInNanoSeconds property
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "PeriodInNanoSeconds" -ErrorAction SilentlyContinue
 
--- test.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Disabling PowerShell 7 Telemetry..." -ForegroundColor Yellow
-
-# Sets the environment variable globally at the Machine level
-[Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
-
-# Updates the current running script session immediately so you don't have to restart the terminal
-$env:POWERSHELL_TELEMETRY_OPTOUT = '1'
-
-Write-Host "PowerShell 7 Telemetry disabled successfully." -ForegroundColor Green
