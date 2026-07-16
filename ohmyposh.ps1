18977b83746c04bc5d0cb92779983437841c4d9e
Myles Mattlock <myles.mattlock@outlook.com>
2026-07-16 14:27:17 +0100

    fixes and cleanup tool added

--- ohmyposh.ps1
+++ ohmyposh.ps1
@@ -1,4 +1,1 @@
-if (-not (Get-Command pwsh)) {
-    Write-Host "Powershell 7 not found installing..."
-    Install-WinUtilWinget
-    winget install Microsoft.PowerShell --source winget --silentwt new-tab pwsh -NoExit -Command "irm https://github.com/Myles-Mattlock/ohmyposh/raw/main/setup.ps1 | iex"
+wt new-tab pwsh -NoExit -Command "irm https://github.com/Myles-Mattlock/ohmyposh/raw/main/setup.ps1 | iex"
